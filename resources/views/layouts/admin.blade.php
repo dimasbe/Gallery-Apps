b5df675878e13f313c988416290b8af1bcd0931e
dimasbe <bedimas55@gmail.com>
2025-05-28 11:08:19 +0700

    admin layout fix

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -335,13 +335,14 @@
 
         {{-- MAIN CONTENT --}}
         <main id="mainContent" class="flex-grow p-6 overflow-y-auto bg-custom-main-bg">
-                @yield('content')
+            @yield('content')
 
-                {{-- FOOTER --}}
-                <div class="flex justify-between items-center text-gray-500 text-sm pt-8 border-t border-gray-200 mt-8">
-                    <p>2025 Hummatech All Rights Reserved</p>
-                    <p>Copyright By GalleryApp</p>
-                </div>
+            {{-- FOOTER --}}
+            <div class="flex justify-between items-center text-gray-500 text-sm border-t border-gray-100 mt-4 bg-white px-6 py-1"
+            style="margin-left: -1.5rem; margin-right: -1.5rem; width: calc(100% + 3rem);">
+            <p>2025 Hummatech All Rights Reserved</p>
+            <p>Copyright By GalleryApp</p>
+        </div>
         </main>
     </div>
 
